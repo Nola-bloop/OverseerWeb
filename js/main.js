443b5205a993b7ce2d6ade6b4c465633c100aa90
Nola <Nola.Bloop@gmail.com>
2026-01-17 18:31:17 -0500

    fast commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let API_URL = "http://107.152.41.172:8889"
 
 let CAMPAIGN = localStorage.getItem("overseer-campaign")
+let CHAPTER
 let USER = localStorage.getItem("overseer-user")
 let PASSWORD = localStorage.getItem("overseer-password")
 let AUTHENTICATED = false
@@ -76,8 +77,96 @@ function toggleEditMode(scope){
 	}
 }
 
-function addChapterGroupToUI(name, chapters){
+function addChapterGroupToUI(chapterGroup){
+	let element = document.createElement("div")
+	element.classList.add("chapter-group")
 
+	element.innerHTML = 
+	`
+	<div class="chapter-group-header" onclick="selectElementsFromId('${chapterGroup.id}')">
+		<p class="chapter-group-title" >${chapterGroup.name}</p>
+		<p class="chapter-group-arrow selectable-${chapterGroup.id}">➤</p>
+	</div>
+	<div id="chapter-container-${chapterGroup.id}" class="chapter-container selectable-${chapterGroup.id}">
+	</div>
+	`
+
+	document.getElementById("aside-chapter-group-container").appendChild(element)
+}
+
+function addChapterToUI(chapter, chapterGroupId){
+	let chapterContainer = document.getElementById(`chapter-container-${chapterGroupId}`)
+	let element = document.createElement("div")
+	element.classList.add("chapter")
+
+	element.innerHTML = `<p id="${chapter.id}" class="chapter-title" onclick="selectChapter(${chapter.id})">${chapter.name}</p>`
+
+	chapterContainer.appendChild(element)
+}
+
+function getChapterFromId(chapterId){
+	for (let i = 0; i < CAMPAIGN.chapter_groups.length; i++){
+		let chapterGroup = CAMPAIGN.chapter_groups[i]
+		for (let j = 0; j < chapterGroup.chapters.length; j++){
+			if (chapterGroup.chapters[j].id === chapterId) return chapterGroup.chapters[j]
+		}
+	}
+}
+
+function selectChapter(id){
+	CHAPTER = getChapterFromId(id)
+
+	if (!CHAPTER){console.log("No chapter with id "+id); return}
+	refreshChapter()
+}
+
+function addMessage(message){
+	let readingZone = document.getElementById("reading-zone")
+
+	let pfp = message.speaker.profile_picture ?? "./media/pfps/unknown.png"
+
+	let messageHTML = `
+	<div class="message">
+		<div class="message-header">
+			<img src="${pfp}" alt="profile picture" class="message-profile-picture">
+			<p class="message-author">${message.speaker.name}</p>
+		</div>
+		<p class="message-text">${message.message}</p>
+	</div>
+	`
+	readingZone.innerHTML+=messageHTML
+}
+
+function refreshChapter(){
+	let readingZone = document.getElementById("reading-zone")
+	readingZone.innerHTML = ""
+
+	for (let i = 0; i < CHAPTER.messages.length; i++){
+		addMessage(CHAPTER.messages[i])
+	}
+}
+function refreshCampaign(){
+	//set campaign name everywhere it needs to be
+	document.title = `${CAMPAIGN.name} - (Overseer)`
+	let campaignTitles = document.getElementsByClassName("campaign-name")
+	for(let i = 0; i < campaignTitles.length; i++){
+		campaignTitles[i].innerHTML = CAMPAIGN.name
+	}
+
+
+
+	//load chapter groups
+	for (let i = 0; i < CAMPAIGN.chapter_groups.length; i++){
+		addChapterGroupToUI(CAMPAIGN.chapter_groups[i])
+
+		//load chapters
+		for (let j = 0; j < CAMPAIGN.chapter_groups[i].chapters.length; j++){
+			addChapterToUI(CAMPAIGN.chapter_groups[i].chapters[j], CAMPAIGN.chapter_groups[i].id)
+		}
+	}
+
+	CHAPTER = CAMPAIGN.chapter_groups[0].chapters[0]
+	refreshChapter()
 }
 
 
@@ -85,11 +174,15 @@ function addChapterGroupToUI(name, chapters){
 
 
 //LOGIC =================>
+//load test values
+CAMPAIGN = TEST_VALUES[0]
+
+
 if (USER && PASSWORD) AUTHENTICATED = authenticate(USER.dc_user_id, PASSWORD) //async
 
 //set accurate page title if needed.
-if (CAMPAIGN)
-	document.title = `${CAMPAIGN.name} - (Overseer)`
-else
-	document.title = `Overseer`
+if (CAMPAIGN){
+	refreshCampaign()
+}
+else document.title = `Overseer`
 
---
+++ js/test-values.js
@@ -0,0 +1,103 @@
+const TEST_VALUES = [
+	//campaign
+	{
+		name : "Valmora",
+		uid : "052e3aaf-ca11-4bac-b9be-98d1461b69ee",
+		owners: [
+			{
+				dc_user_id : "300012833016512514",
+				dc_username: "Nola",
+				password_hash: "some_password_hash"
+			},
+			{
+				dc_user_id : "1290040130622591038",
+				dc_username: "sy",
+				password_hash: "some_password_hash"
+			},
+		],
+		chapter_groups : [
+			{
+				id: 1,
+				name:"Story",
+				chapters: [
+					{
+						id: 1,
+						uid: "969e6c8f-d72d-4ca9-9c9a-7e9c6fa4c283",
+						name: "Prologue",
+						isCanon: true,
+						messages: [
+							{
+								id:1,
+								message:`After the prince’s speech, a weight settles on your eyelids, beckoning them closed. The light beating down on you from the chandeliers lining the ceilings feels hot on your skin, far from the inviting warmth you had imagined. A scent lingers as you drift back to sleep… sage, lilies, and something else. \n\nThree knocks on hard wood startle you awake. Your body stings in shock from the sensation of the mattress beneath you, quilted, featherlight, and soft. Mindnumbingly soft. You are a millimeter from falling into slumber once more, when you hear the door swing open. Followed by footsteps. \n\n“Prisoner,” the voice calls. “It’s time.”`,
+								chapter:"1",
+								speaker:{
+									id: 1,
+									name: "Oracle",
+									boon: "None.",
+									bio: "None.",
+									player: 1
+								},
+								date_sent:"9/30/25",
+								thread: "main"
+							},
+							{
+								id:2,
+								message:`*Arya, Éponine, and Guinevere must roll for perception.*`,
+								chapter:"1",
+								speaker:{
+									id: 2,
+									name: "Fate",
+									boon: "None.",
+									bio: "None.",
+									player: 1
+								},
+								date_sent:"9/30/25",
+								thread: "main"
+							},
+						]
+					},
+					{
+						id: 2,
+						uid: "969e6c8f-d72d-4ca9-9c9a-7e9c6fa4c283",
+						name: "Chapter 1",
+						isCanon: true,
+						messages: "unloaded"
+					},
+					{
+						id: 3,
+						uid: "969e6c8f-d72d-4ca9-9c9a-7e9c6fa4c283",
+						name: "Chapter 2",
+						isCanon: true,
+						messages: "unloaded"
+					},
+					{
+						id: 4,
+						uid: "969e6c8f-d72d-4ca9-9c9a-7e9c6fa4c283",
+						name: "Chapter 3",
+						isCanon: true,
+						messages: "unloaded"
+					},
+					{
+						id: 5,
+						uid: "969e6c8f-d72d-4ca9-9c9a-7e9c6fa4c283",
+						name: "Chapter 4",
+						isCanon: true,
+						messages: "unloaded"
+					},
+				]
+			},
+			{
+				id: 2,
+				name:"Lore & info",
+				chapters: [
+					{
+						uid: "1bc69f64-fd18-4683-8334-b5b77ecf46fd",
+						name: "Prologue",
+						isCanon: false,
+						messages: "unloaded"
+					},
+				]
+			},
+		]
+	}
+]
